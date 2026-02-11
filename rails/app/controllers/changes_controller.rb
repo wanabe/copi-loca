9ac700149a14cf2f033f755fc6bf2c3f0fea6a42
wanabe <s.wanabe@gmail.com>
2026-02-11 11:01:08 +0900

    Add ChangesController#revert and shorthand redirect from '/r'

---
+++ rails/app/controllers/changes_controller.rb
@@ -0,0 +1,14 @@
+class ChangesController < ApplicationController
+  def revert
+  end
+
+  def execute_revert
+    client = Client.instance.copilot_client
+    client.create_session(model: "gpt-4.1") do |session|
+      session.send("Please execute `git checkout . && git clean -fd`")
+      client.wait { session.idle? }
+    end
+    
+    redirect_to root_path, notice: 'Code changes reverted.'
+  end
+end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
   root to: 'sessions#index'
 
+  get 'r', to: 'changes#revert'
+  resources :changes, only: [] do
+    collection do
+      get :revert
+      post :execute_revert
+    end
+  end
+
   resources :sessions, only: [:index, :show, :create, :destroy] do
     resources :messages, only: [:index, :create]
     resources :rpc_logs, only: [:index, :show]
