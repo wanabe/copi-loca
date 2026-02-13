f500239005e91c23f2fee3288d640efee494095b
wanabe <s.wanabe@gmail.com>
2026-02-13 03:07:35 +0000

    Files: display directory tree using git ls-files, add partial for recursive rendering. Breadcrumbs: improve session/message/rpc log ID display. .gitignore: exclude vendor/bundle.

--- rails/app/controllers/files_controller.rb
+++ rails/app/controllers/files_controller.rb
@@ -1,5 +1,25 @@
 class FilesController < ApplicationController
   def index
-    @files = `git ls-files -c -o --exclude-standard`.chomp.split("\n")
+    files = `git ls-files -c -o --exclude-standard`.chomp.split("\n")
+    @tree = build_tree(files)
+  end
+
+  private
+
+  def build_tree(paths)
+    tree = {}
+    paths.each do |path|
+      parts = path.split('/')
+      current = tree
+      parts.each_with_index do |part, i|
+        if i == parts.size - 1
+          (current[:files] ||= []) << part
+        else
+          current[:dirs] ||= {}
+          current = (current[:dirs][part] ||= {})
+        end
+      end
+    end
+    tree
   end
 end
