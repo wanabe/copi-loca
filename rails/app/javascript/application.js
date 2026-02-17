adea7106727631b52d444e8a43db3a04e9860e59
wanabe <s.wanabe@gmail.com>
2026-02-17 12:32:33 +0900

    Introduce Prism.js for changes pages

--- rails/app/javascript/application.js
+++ rails/app/javascript/application.js
@@ -1,3 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// Prism.js再初期化: Turboページ遷移後にもハイライトを適用
+if (window.Prism) {
+  document.addEventListener('turbo:load', function() {
+    window.Prism.highlightAll();
+  });
+}
+
