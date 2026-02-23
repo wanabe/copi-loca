27ccfeb1d57d4542dbcecb6c911511b8a9370394
wanabe <s.wanabe@gmail.com>
2026-02-23 10:38:47 +0900

    Exclude safety guard from coverage

--- rails/spec/rails_helper.rb
+++ rails/spec/rails_helper.rb
@@ -36,7 +36,9 @@ ApplicationController.prepend(StubAuthenticate)
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
+  # :nocov:
   abort e.to_s.strip
+  # :nocov:
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
