a681e3b4753b51b8fc5354a3667f30ccc1ec7157
wanabe <s.wanabe@gmail.com>
2026-03-02 17:53:10 +0900

    Fix Operation#run to clear bundler setting for process

--- rails/app/models/operation.rb
+++ rails/app/models/operation.rb
@@ -6,23 +6,18 @@ class Operation < ApplicationRecord
 
   enum :execution_timing, { manual: 1, immediate: 2, background: 3 }
 
-  def run
+  def run(&)
     unless block_given?
       output = ""
       status = run { |reader| output = reader.read }
       return output, status
     end
 
-    status = nil
     Dir.chdir(directory) do
-      reader, writer = IO.pipe
-      pid = Process.spawn(command, out: writer, err: writer)
-      writer.close
-      yield reader
-      _, process_status = Process.wait2(pid)
-      status = process_status.exitstatus
-      reader.close
+      Bundler.with_unbundled_env do
+        IO.popen(command, &)
+      end
     end
-    status
+    Process.last_status.exitstatus
   end
 end
