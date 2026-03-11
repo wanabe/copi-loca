6e9b034d992ffe456e0d9b68ff0d290d8b91d229
wanabe <s.wanabe@gmail.com>
2026-03-12 03:13:55 +0900

    Add spec/views/layouts/application_spec.rb

---
+++ spec/views/layouts/application_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Views::Layouts::Application do
+  subject(:rendered) { render described_class.new(title: title) {} }
+
+  context "with title" do
+    let(:title) { "Test Title" }
+
+    it "renders the layout with the given title" do
+      expect(rendered).to include("<title>Test Title</title>")
+      expect(rendered).to include("<meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">")
+      expect(rendered).to include("Copi Loca")
+      expect(rendered).to include("<link rel=\"stylesheet\"")
+      expect(rendered).to include("<script type=\"importmap\"")
+    end
+  end
+
+  context "without title" do
+    let(:title) { nil }
+
+    it "renders the layout with default title if none given" do
+      expect(rendered).to include("<title>Copi Loca</title>")
+    end
+  end
+end
