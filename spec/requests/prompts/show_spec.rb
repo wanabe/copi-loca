84ff853a6574f557f99aa70c22d9b059bd2a41b1
wanabe <s.wanabe@gmail.com>
2026-03-12 04:33:49 +0900

    Add spec/requests/prompts/show_spec.rb

---
+++ spec/requests/prompts/show_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'GET /prompts/:id' do
+  it 'renders the show view for the prompt' do
+    prompt = Prompt.new(id: 1, text: 'Test prompt')
+    allow(prompt).to receive(:response).and_return(nil)
+    allow(Prompt).to receive(:find).with('1').and_return(prompt)
+
+    get prompt_path(1)
+
+    expect(controller).to be_a(PromptsController)
+    expect(response).to have_http_status(:ok)
+    expect(response.body).to include('Test prompt')
+    expect(response.body).to include('Edit this prompt')
+    expect(response.body).to include('Back to prompts')
+    expect(response.body).to include('Run this prompt')
+    expect(response.body).to include('Destroy this prompt')
+  end
+end
