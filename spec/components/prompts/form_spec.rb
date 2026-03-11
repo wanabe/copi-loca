3c9fb7171928ad5645f8fcd76fadf164d8468d96
wanabe <s.wanabe@gmail.com>
2026-03-12 03:46:55 +0900

    Add spec/components/prompts/form_spec.rb

---
+++ spec/components/prompts/form_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Components::Prompts::Form do
+  let(:prompt) { Prompt.new(id: 1, text: 'Sample prompt') }
+
+  subject(:rendered) { render described_class.new(prompt: prompt) }
+
+  describe '#view_template' do
+    it 'renders the form with id and text fields' do
+      expect(rendered).to include('form')
+      expect(rendered).to include('id')
+      expect(rendered).to include('text')
+      expect(rendered).to include('submit')
+    end
+
+    it 'displays errors when present' do
+      prompt.errors.add(:text, "can't be blank")
+      rendered_with_errors = render described_class.new(prompt: prompt)
+      expect(rendered_with_errors).to include('prohibited this prompt from being saved')
+      expect(rendered_with_errors).to include(ERB::Util.html_escape("can't be blank"))
+    end
+  end
+end
