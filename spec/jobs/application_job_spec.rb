86eee474ec7af972d8f9319fcf052f957ac9689c
wanabe <s.wanabe@gmail.com>
2026-03-12 03:35:50 +0900

    Add spec/jobs/application_job_spec.rb

---
+++ spec/jobs/application_job_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ApplicationJob do
+  describe 'inheritance' do
+    it 'inherits from ActiveJob::Base' do
+      expect(ApplicationJob < ActiveJob::Base).to be true
+    end
+  end
+
+  describe 'retry_on deadlock' do
+    it 'has commented retry_on for ActiveRecord::Deadlocked' do
+      source = File.read(Rails.root.join('app/jobs/application_job.rb'))
+      expect(source).to include('retry_on ActiveRecord::Deadlocked')
+    end
+  end
+
+  describe 'discard_on deserialization error' do
+    it 'has commented discard_on for ActiveJob::DeserializationError' do
+      source = File.read(Rails.root.join('app/jobs/application_job.rb'))
+      expect(source).to include('discard_on ActiveJob::DeserializationError')
+    end
+  end
+end
